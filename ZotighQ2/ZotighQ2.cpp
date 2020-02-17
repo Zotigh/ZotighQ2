a7f00c9c73e013fb6bbbd1745a3cab4a4085f78a
Lance Zotigh <lancezotighlz@gmail.com>
2020-02-17 15:12:40 -0700

    Added header greeter

--- ZotighQ2/ZotighQ2.cpp
+++ ZotighQ2/ZotighQ2.cpp
@@ -14,4 +14,12 @@ int main()
 	int weather;
 	int temp;
 	int i = 1;
+
+	// Header/Greeting cout's
+	cout << "Lance Zotigh" << endl;
+	cout << "Quiz #2 Vacation Planner" << endl;
+	cout << "\nObjective: A C++ program that will help a person going on vacation to the ocean to determine whether they should to go to the beach or visit the tourist spots." << endl;
+	cout << "Welcome!" << endl;
+
+
 }
